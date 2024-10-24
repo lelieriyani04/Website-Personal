3569178b9ea4e56e826507e8aacd72b921132c7b
lelieriyani04 <lelieriyanihasanah@gmail.com>
2024-10-25 06:56:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// sticky navbar
+window.onscroll = function(){
+
+    var navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 55){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+
+    // scroll to top
+    let button = document.getElementById("btn");
+
+    if(document.documentElement.scrollTop > 90){
+        button.style.display = "block";
+    }
+    else{
+        button.style.display = "none";
+    }
+
+};
+
+function scrolltoTop(){
+    document.documentElement.scrollTop = 0;
+}
+
+// menu toggle bar
+function menu(){
+    let menu = document.getElementById("menu");
+    let menubar = document.querySelector(".menu-btn i");
+
+    menu.classList.toggle("active");
+    menubar.classList.toggle("active");
+}
